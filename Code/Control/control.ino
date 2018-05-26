940d881070e8c112b054f94dee07c91794844887
Jason Too <tooyipjee@gmail.com>
2018-05-26 13:41:56 +0100

    Code with most functionality - not tested
    Added a "PWM" that changes value of tau from 0 - 1000, which would determine the proportion of time the valve is open.
    
    Using wikipedia, assumed upper bound to be around 20000 lux and lower bound to be around 1000 lux.
    
    Removed some bugs.

--- Code/Control/control.ino
+++ Code/Control/control.ino
@@ -1,20 +1,41 @@
 #include <Wire.h>
 #include <BH1750.h>
 
+//INITIALISATION
 //Pin Assignments
-int solenoidPin = 2;
+int dviPin = 4;
+int enPin = 5;
+int intPin = 6;
+int sPin1 = 8;
+int sPin2 = 9;
+int sPin3 = 10;
+int sPin4 = 11;
+int debugPin1 = 12;
+int debugPin2 = 13;
 int analogPin1 = A0;
-int analogPin2=A1;
+int analogPin2 = A1;
 
 BH1750 lightMeter;
-int val1=0;
-int val2=0;
+int lbound = 0;
+int ubound = 0;
+int tau = 0;
+uint16_t lux = 0;
 
-void setup() {
+//SETUP
+void setup() 
+{
   Serial.begin(9600);
-  
+
   // put your setup code here, to run once:
-  pinMode(solenoidPin, OUTPUT);
+  pinMode(dviPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  pinMode(intPin, OUTPUT);
+  pinMode(sPin1, OUTPUT);
+  pinMode(sPin2, OUTPUT);
+  pinMode(sPin3, OUTPUT);
+  pinMode(sPin4, OUTPUT);
+  pinMode(debugPin1, OUTPUT);
+  pinMode(debugPin2, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
@@ -22,31 +43,59 @@ void setup() {
   Wire.begin();
   lightMeter.begin();
   Serial.println(F("BH1750 Test"));
+
+  //Setup of valve is OFF
+  digitalWrite(sPin1, LOW);
+  digitalWrite(sPin2, LOW);
+  digitalWrite(sPin3, LOW);
+  digitalWrite(sPin4, LOW);
+
+  //tuning circuit
+  lbound = analogRead(analogPin1) + 500; // lower bound from 500 - 1500
+  ubound = (analogRead(analogPin2)*2) + 19000; //upper bound from 19000 - 21000
 }
 
-void loop() {
-  //Reading knobs
-  val1 = analogRead(analogPin1);
-  val2 = analogRead(analogPin2);
-  serial.println(val1);
-  serial.println(val2);
-  
-  //BH1750
-  uint16_t lux = lightMeter.readLightLevel();
+//LOOP
+void loop() 
+{
+  //BH1750 - reading light levels
+  lux = lightMeter.readLightLevel();
   Serial.print("Light: ");
   Serial.print(lux);
-  Serial.println(" lx");
-  delay(1000);
+  //Serial.println(" lx");
   
-  //solenoid
-  digitalWrite(solenoidPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(solenoidPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);
+  //Reading knobs & calculating tau wrt to lux levels
+  if(lux > ubound)
+  {
+    tau = 1000;
+  }
+  else
+  {
+    tau = 1000*(lux/ubound);
+  };
+  //serial.println(val1);
+  //serial.println(val2);
+
+  //control for solenoid
+  //TO BE COMPLETED
+  if(tau<lbound)
+  {
+    //turning solenoid off
+    digitalWrite(sPin2, LOW);
+    digitalWrite(sPin3, LOW);
+  }
+  else
+  {
+    //turning on with some PWM
+    digitalWrite(sPin2, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(tau);                       // wait for a second
+    digitalWrite(sPin3, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000-tau);
+  };
   
   //blink
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);          
+  delay(1000);                       // wait for a second
 }
