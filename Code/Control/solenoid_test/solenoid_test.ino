f856534d3c5ba06caaea9acf83da028250fa5bf9
Jason Too <tooyipjee@gmail.com>
2018-05-28 17:43:46 +0100

    Test circuit for solenoid and light

--- Code/Control/solenoid_test/solenoid_test.ino
+++ Code/Control/solenoid_test/solenoid_test.ino
@@ -1,8 +1,26 @@
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter(0x5c);
+
+//INITIALISATION
 //Pin Assignments
+int dviPin = 4;
+int enPin = 5;
+int intPin = 6;
+int sPin1 = 8;
 int sPin2 = 9;
 int sPin3 = 10;
-
-int tau = 0;
+int sPin4 = 11;
+int debugPin1 = 12;
+int debugPin2 = 13;
+int analogPin1 = A0;
+int analogPin2 = A1;
+//Variables
+float lbound = 0;
+float ubound = 0;
+float tau = 0;
+uint16_t lux = 0;
 
 //SETUP
 void setup() 
@@ -10,20 +28,54 @@ void setup()
   Serial.begin(9600);
 
   // put your setup code here, to run once:
+  pinMode(dviPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  pinMode(intPin, OUTPUT);
+  pinMode(sPin1, OUTPUT);
+  pinMode(sPin2, OUTPUT);
+  pinMode(sPin3, OUTPUT);
+  pinMode(sPin4, OUTPUT);
+  pinMode(debugPin1, OUTPUT);
+  pinMode(debugPin2, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
+  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
+  // On esp8266 devices you can select SCL and SDA pins using Wire.begin(D4, D3);
+  Wire.begin();
+  lightMeter.begin();
+  Serial.println(F("BH1750 Test"));
+
   //Setup of valve is OFF
+  digitalWrite(sPin1, LOW);
   digitalWrite(sPin2, LOW);
+  digitalWrite(sPin3, LOW);
+  digitalWrite(sPin4, LOW);
 }
 
 //LOOP
 void loop() 
-{
-  tau=1000;
-  digitalWrite(sPin3, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH); 
-  delay(tau);                       
-  digitalWrite(sPin3, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(2000-tau);
+{ 
+  //BH1750 - reading light levels
+  lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.println(lux);
+  
+  //Reading knobs & calculating tau wrt to lux levels
+  if(lux > 300)
+  {
+    tau = 1000;
+    digitalWrite(sPin2, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH); 
+    delay(tau);                       
+    digitalWrite(sPin2, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2000-tau);
+  } else 
+  {
+    //turning solenoid off
+    digitalWrite(sPin2, LOW);
+    digitalWrite(sPin3, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2000);
+  };
 }
