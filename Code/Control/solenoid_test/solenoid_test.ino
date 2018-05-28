9cc09d5681914958d6a0e0376adbc4dd32f5480f
Jason Too <tooyipjee@gmail.com>
2018-05-28 16:31:23 +0100

    Test code

--- Code/Control/solenoid_test/solenoid_test.ino
+++ Code/Control/solenoid_test/solenoid_test.ino
@@ -1,11 +1,8 @@
 //Pin Assignments
 int sPin2 = 9;
 int sPin3 = 10;
-//Variables
-float lbound = 500;
-float ubound = 19000;
-float tau = 0;
-uint16_t lux = 0;
+
+int tau = 0;
 
 //SETUP
 void setup() 
@@ -17,39 +14,16 @@ void setup()
 
   //Setup of valve is OFF
   digitalWrite(sPin2, LOW);
-  digitalWrite(sPin3, LOW);
 }
 
 //LOOP
 void loop() 
 {
-  //reading light levels
-  lux = 5000;
-  
-  //Reading knobs & calculating tau wrt to lux levels
-  if(lux > ubound)
-  {
-    tau = 1000;
-  } else
-  {
-    tau = (1000*(lux/(ubound+lbound)));
-  };
-  
-  //control for solenoid
-  if(lux<lbound)
-  {
-    //turning solenoid off
-    digitalWrite(sPin2, LOW);
-    digitalWrite(sPin3, LOW);
-    digitalWrite(LED_BUILTIN, LOW); 
-  } else
-  {
-    //turning on with some PWM
-    digitalWrite(sPin2, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH); 
-    delay(tau);                       
-    digitalWrite(sPin3, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000-tau);
-  };
+  tau=1000;
+  digitalWrite(sPin3, HIGH);
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(tau);                       
+  digitalWrite(sPin3, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000-tau);
 }
